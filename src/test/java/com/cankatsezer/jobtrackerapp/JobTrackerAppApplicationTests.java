aaee9b1f9a7fda0acc5c49b1dc2cdd48d66cc57e
Cankat Sezer <cankat_sezer055@hotmail.com>
2023-07-20 01:23:26 +0300

    Initial commit

---
+++ src/test/java/com/cankatsezer/jobtrackerapp/JobTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cankatsezer.jobtrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
